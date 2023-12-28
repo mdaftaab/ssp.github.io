5037163503c0c7d32c1ce9b88cb03b6fdd35e792
Md Aftaab <45416601+mdaftaab@users.noreply.github.com>
2023-12-28 18:21:00 +0530

    add all important files

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+var swiper = new Swiper(".serviceSwiper", {
+	// loop: true,
+	slidesPerView: 3.5,
+	spaceBetween:20,
+	// centeredSlides: true,
+	// pagination: {
+	//   el: ".swiper-pagination",
+	//   clickable: true,
+	// },
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".certificationSwiper", {
+	loop: true,
+	slidesPerView: 4,
+	spaceBetween:20,
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".patentsSwiper", {
+	// loop: true,
+	slidesPerView: 4,
+	spaceBetween:20,
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".timelineSwiper", {
+	// loop: true,
+	slidesPerView: 4,
+	spaceBetween:0,
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+$('#counter').waypoint(function() {
+    // Counter animation code
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    }, { offset: '80%' });
+}, { offset: '80%' });
